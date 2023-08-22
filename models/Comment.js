94b2d9c60d60340cf3ff3b61d5521307ca2c9113
praveshpansari <praveshpansari@gmail.com>
2023-08-22 15:55:55 +0530

    sequelize connection

---
+++ models/Comment.js
@@ -0,0 +1,26 @@
+import sequelize from "../sequelize.js";
+import { DataTypes } from "sequelize";
+
+const Comment = sequelize.define("comments", {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    primaryKey: true,
+  },
+  parent_id: {
+    type: DataTypes.INTEGER,
+    allowNull: true,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+  comment: {
+    type: DataTypes.STRING,
+    allowNull: false,
+  },
+});
+
+Comment.belongsTo(Comment, { foreignKey: "parent_id", as: "Parent" });
+
+export default Comment;
